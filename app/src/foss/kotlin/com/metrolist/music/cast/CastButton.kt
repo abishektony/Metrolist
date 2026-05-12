817f4b13e19d304a6a6101dafa71f9e4304bb54d
abishektony <139079623+abishektony@users.noreply.github.com>
2026-05-11 20:38:53 -0400

    release

--- app/src/foss/kotlin/com/metrolist/music/cast/CastButton.kt
+++
@@ -1,9 +0,0 @@
-package com.metrolist.music.cast
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun CastButton(modifier: Modifier = Modifier) {
-    // No-op for FOSS build
-}
